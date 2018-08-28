e1429b8a72f2d475600fb32b961800637dd1ebed
Josh <kreativejosh@gmail.com>
2018-08-28 07:38:31 -0700

    test

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,17 +4,21 @@
    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Ezinne</title>
+     <title>Agrocity</title>
 	<meta charset="utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/reset.css') }}">
+      <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}">
+       <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/styless.css') }}">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/amara.css') }}">
         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/josh.css') }}">
 
 </head>
 <body>
+  
     @include('layout.nav')
     @yield('content')
     @include('layout.footer')
