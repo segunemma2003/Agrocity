73df10e5a34e6f655c3ac06a2c518f3711a524ec
segunemma2003 <segunemma2003@gmail.com>
2018-09-04 14:28:24 +0100

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-//         if ($this->app->environment() == 'production') {
-//     URL::forceScheme('https');
-// }
+        if ($this->app->environment() == 'production') {
+    URL::forceScheme('https');
+}
     }
 
     /**
