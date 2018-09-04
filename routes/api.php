8f2243590e6ab51e503badb1566a6d2f48df059e
segunemma2003 <segunemma2003@gmail.com>
2018-09-04 22:04:06 +0100

    heroku

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::post('/register','FarmersController@api_create');
+Route::post('/login','FarmersController@api_login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
