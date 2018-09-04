9fea2727d856ae58f862bcd850817e87377f98c3
segunemma2003 <segunemma2003@gmail.com>
2018-09-04 22:28:25 +0100

    heroku

--- app/Http/Controllers/FarmersController.php
+++ app/Http/Controllers/FarmersController.php
@@ -48,7 +48,7 @@ class FarmersController extends Controller
         }
     }
     public function api_login(Request $request){
-        $user=User::whereEmail($request->email)->wherePassword(Hash::make($request->password))->get();
+        $user=User::whereEmail($request->email)->first();
         // dd($user);
         if($user){
             // dd($user->verified);
